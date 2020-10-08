911a1b4c0f4c442a03aa42099826acb56cd49011
Shashank Verma <71495535+shashankdasverma@users.noreply.github.com>
2020-10-08 12:13:24 +0545

    Create interchange.asm

---
+++ interchange.asm
@@ -0,0 +1,24 @@
+.model small
+ .data
+    num1 db 00h,03h,42h,98h
+    num2 db 07h,57h,13h,64h
+    res db 4 dup(?)                         
+    
+                                 
+
+  .code
+    mov ax,@data
+    mov ds,ax
+    
+     mov si,03h
+     mov cl,04h
+     clc
+ deciadd: mov al,num1[si]
+          adc al,num2[si]
+          daa
+          mov res[si],al
+          dec si
+          loop deciadd
+          mov ah,4ch
+          int 21h
+          end
